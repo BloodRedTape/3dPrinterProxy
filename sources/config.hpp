83c37d8e3a141220bf6f32d2376aa9c8740f212c
I.Kostiuk <gefestroyale@gmail.com>
2025-06-13 13:58:19 +0300

    Add logging

---
+++ sources/config.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "pch/std.hpp"
+
+struct Config {
+	static std::int64_t LogChat;
+	static std::int64_t LogTopic;
+	static std::string LogToken;
+	static std::string DebugBotName;
+	static bool LogIsEnabled;
+};
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,24 @@
 #include <filesystem>
 #include <bsl/log.hpp>
 #include "printer_proxy.hpp"
+#include "simple/tg_logger.hpp"
+#include "config.hpp"
+
+std::unique_ptr<SimpleTgLogger> s_Logger;
+
+void LogFunctionExternal(const std::string& category, Verbosity verbosity, const std::string& message) {
+	if(!s_Logger)
+		return;
+
+	auto line = Format("[%][%]: %", category, verbosity, message);
+
+	Println("%", line);
+
+	if(verbosity <= Verbosity::Display)
+		return;
+	
+	s_Logger->Log(line);
+}
 
 int main(int argc, char* argv[])
 {
@@ -8,10 +26,15 @@ int main(int argc, char* argv[])
         std::filesystem::current_path(argv[1]);
     }
 
+	s_Logger = std::make_unique<SimpleTgLogger>(Config::LogToken, Config::LogChat, Config::DebugBotName, Config::LogTopic);
+	s_Logger->SetEnabled(Config::LogIsEnabled);
+
     PrinterProxy proxy;
     proxy.Listen(2228);
 
     proxy.RunAsync();
+
+	Log("Main", Info, "Started");
 
     Async::Run();
 
